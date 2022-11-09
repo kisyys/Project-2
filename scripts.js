1496b76cfebdec0f0251c636bc4b04078adcf1d3
Kimmo S <kisyys22@gmail.com>
2022-11-10 01:00:29 +0200

    Updates

--- scripts.js
+++ scripts.js
@@ -56,31 +56,49 @@ getTheatre = () => {
 }
 
 getData = () => {
+    document.getElementById("movie_data1").innerHTML = "";
+    document.getElementById("movie_data2").innerHTML = "";
+    document.getElementById("movie_data3").innerHTML = "";
     let theatre = document.getElementById("theater_list").value;
     let date = document.getElementById("date_list").value;
     console.log(theatre);
-    // Create AJAX object
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.overrideMimeType('application/xml');
+    // Create AJAX objects
+    let xmlhttp1 = new XMLHttpRequest();
+    xmlhttp1.overrideMimeType('application/xml');
+    let xmlhttp2 = new XMLHttpRequest();
+    xmlhttp2.overrideMimeType('application/xml');
 
     // Specify the data / url to be fetched
     let URL2 = "https://www.finnkino.fi/xml/Schedule/?area=" + theatre + "&dt=" + date;
-    xmlhttp.open("GET", URL2, true);
-    xmlhttp.send();
+    xmlhttp1.open("GET", URL2, true);
+    xmlhttp1.send();
 
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    let URL3 = "https://www.finnkino.fi/xml/Events/";
+    xmlhttp2.open("GET", URL3, true);
+    xmlhttp2.send();
+
+    xmlhttp1.onreadystatechange, xmlhttp2.onreadystatechange = function () {
+        if (xmlhttp1.readyState == 4 && xmlhttp1.status == 200 && xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {
             // find myDiv and insert results there
-            document.getElementById("movie_data").innerHTML = "";
-
-            for (let i = 0; i < xmlhttp.responseXML.getElementsByTagName("Show").length; i++) {
-                let title = xmlhttp.responseXML.getElementsByTagName("Title")[i].innerHTML;
-                let pic = xmlhttp.responseXML.getElementsByTagName("EventSmallImagePortrait")[i].innerHTML;
-                let start = xmlhttp.responseXML.getElementsByTagName("dttmShowStart")[i].innerHTML.split("T");
+            
+            for (let i = 0; i < xmlhttp1.responseXML.getElementsByTagName("Show").length; i++) {
+                let ID = xmlhttp1.responseXML.getElementsByTagName("EventID")[i].innerHTML;
+                let title = xmlhttp1.responseXML.getElementsByTagName("Title")[i].innerHTML;
+                let pic = xmlhttp1.responseXML.getElementsByTagName("EventSmallImagePortrait")[i].innerHTML;
+                let start = xmlhttp1.responseXML.getElementsByTagName("dttmShowStart")[i].innerHTML.split("T");
                 let YearMonthDaySplit = start[0].split("-");
-                let place = xmlhttp.responseXML.getElementsByTagName("TheatreAndAuditorium")[i].innerHTML;
+                let place = xmlhttp1.responseXML.getElementsByTagName("TheatreAndAuditorium")[i].innerHTML;
 
-                document.getElementById("movie_data").innerHTML += `<div class="movie"> <b>` + title + "</b> <br>" + place + "<br> <img src=" + pic + " alt=''> " + YearMonthDaySplit[2] + "." + YearMonthDaySplit[1] + "." + YearMonthDaySplit[0] + " " + start[1].slice(0, -3); + " </div> <br> <br>";
+                document.getElementById("movie_data1").innerHTML += `<div class="movie"> <b>` + title + "</b> <br>" + place + "<br>" + YearMonthDaySplit[2] + "." + YearMonthDaySplit[1] + "." + YearMonthDaySplit[0] + " " + start[1].slice(0, -3) + " </div>";
+                
+                document.getElementById("movie_data2").innerHTML += `<div class="movie"> <img src=` + pic + " alt=''> </div>";
+
+                for (let i = 0; i < xmlhttp2.responseXML.getElementsByTagName("ID").length; i++) {
+                    if(parseInt(xmlhttp2.responseXML.getElementsByTagName("ID")[i].innerHTML)===parseInt(ID)) {
+                        document.getElementById("movie_data3").innerHTML += `<div class="movie">` + xmlhttp2.responseXML.getElementsByTagName("ShortSynopsis")[i].innerHTML + "</div>";
+                    }
+                }
+                // ==> Create array list   
             }
         }
     }
